cb623e471620ddb5f21b8a9f758b820aa4833cde
eunssol <enssol01@gmail.com>
2022-02-10 16:53:40 +0900

    test

---
+++ src/main/java/net/ezens/Intranet/dto/Test.java
@@ -0,0 +1,18 @@
+package net.ezens.Intranet.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Test {
+	
+	private String userName;
+	private int userCode;
+	private LocalDateTime hiDay;
+	
+	
+
+}

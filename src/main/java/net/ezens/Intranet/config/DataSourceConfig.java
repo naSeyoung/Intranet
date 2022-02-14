4e6aa4774f86ebc1f99ce2f3a262a2e93f6016a0
chanju.lee <lchj333@ezens.net>
2022-02-14 12:51:10 +0900

    트랜젝션 설정 추가 & 로그 추가

--- src/main/java/net/ezens/Intranet/config/DataSourceConfig.java
+++ src/main/java/net/ezens/Intranet/config/DataSourceConfig.java
@@ -5,13 +5,38 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 @Configuration
 public class DataSourceConfig {
+	
+	private static final Logger logger = LoggerFactory.getLogger(DataSourceConfig.class);
+	
+    @Bean
+    @Primary
+    @Qualifier ("datasource")
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+    	logger.info("=== make dataSource bean ===");
+        return DataSourceBuilder.create().build();
+    }
+    
+    @Bean(name = "tx")
+    public DataSourceTransactionManager tx(@Qualifier ("datasource")DataSource dataSource) {
+    	logger.info("=== make DataSourceTransactionManager bean ===");
+    	DataSourceTransactionManager txm = new DataSourceTransactionManager(dataSource);
+    	return txm;
+    }
 
 	@Bean
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
@@ -38,5 +63,6 @@ public class DataSourceConfig {
 	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory){
 		return new SqlSessionTemplate(sqlSessionFactory);
 	}
+	
 	
 }
